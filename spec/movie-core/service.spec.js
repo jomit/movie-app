d8607741c8db6c1bc1243b6c7e8c93feb5e7fc39
Jomit <jomitv@live.com>
2016-01-19 17:22:14 -0800

    Integrated $httpBackend for mocking http requests

--- spec/movie-core/service.spec.js
+++ spec/movie-core/service.spec.js
@@ -12,6 +12,7 @@ describe("MovieCore", function() {
 
     afterEach(function(){
       $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
     })
 
     
@@ -59,6 +60,39 @@ describe("MovieCore", function() {
        });
 
        popularMovie.$update();
+
+      expect($httpBackend.flush).not.toThrow();
+
+    });
+
+
+    it("should authenticate requests",function(){
+      //var headerData = {authToken: 'teddybear', Accept: 'application/json, text/plain, */*'};
+      var headerData = function(headers) {
+        return headers.authToken === 'teddybear';
+      }
+
+      var matchAny = /.*/;
+
+      $httpBackend.whenGET(matchAny, headerData)
+        .respond(200);
+
+      $httpBackend.expectPOST(matchAny, matchAny, headerData)
+        .respond(200);
+
+      $httpBackend.expectPUT(matchAny, matchAny, headerData)
+        .respond(200);
+
+      $httpBackend.expectDELETE(matchAny, headerData)
+        .respond(200);
+
+      var popularMovie = { id: 'tt0076759', description: 'This movie is great!' };
+
+      PopularMovies.query();
+      PopularMovies.get({ id: 'tt0076759' });
+      new PopularMovies(popularMovie).$save();
+      new PopularMovies(popularMovie).$update();
+      new PopularMovies(popularMovie).$remove();
 
       expect($httpBackend.flush).not.toThrow();
 
--- src/movie-core/service.js
+++ src/movie-core/service.js
@@ -1,12 +1,30 @@
 angular.module("movieCore",["ngResource"])
 .factory("PopularMovies",function ($resource){
+	var token = "teddybear";
 	return $resource("popular/:movieId",
 	{
 		movieId: "@id"
 	},
 	{
 		update: {
-			method: "PUT"
+			method: "PUT",
+			headers: { "authToken": token}
+		},
+		get: {
+			method: "GET",
+			headers: { "authToken": token}
+		},
+		query: {
+			method: "GET",
+			headers: { "authToken": token}
+		},
+		save: {
+			method: "POST",
+			headers: { "authToken": token}
+		},
+		remove: {
+			method: "DELETE",
+			headers: { "authToken": token}
 		}
 	});
 });
