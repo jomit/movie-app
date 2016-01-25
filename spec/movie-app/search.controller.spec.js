e66f0596c72c73f8e080ff4b67ec031f73583388
Jomit <jomitv@live.com>
2016-01-25 12:15:44 -0800

    added tests for $timeout

--- spec/movie-app/search.controller.spec.js
+++ spec/movie-app/search.controller.spec.js
@@ -5,9 +5,10 @@
 
     beforeEach(module("movieApp"));
 
-    beforeEach(inject(function (_$controller_,_$location_) {
+    beforeEach(inject(function (_$controller_,_$location_,_$timeout_) {
         $location = _$location_;
         $controller = _$controller_;
+        $timeout = _$timeout_;
     }));
 
     it("should redirect to the query results page for non-empty query", function () {
@@ -22,5 +23,29 @@
         $this.search();
 
         expect($location.url()).toBe("");
+    });
+
+    it("should redirect after 1 second of keyboard inactivity", function () {
+        $this = $controller("SearchController", { $location: $location, $timeout: $timeout }, { query: "star wars" });
+        $this.keyup();
+        $timeout.flush();
+        expect($timeout.verifyNoPendingTasks).not.toThrow();
+        expect($location.url()).toBe("/results?q=star%20wars");
+    });
+
+    it("should cancel timout in keydown", function () {
+        $this = $controller("SearchController", { $location: $location, $timeout: $timeout }, { query: "star wars" });
+        $this.keyup();
+        $this.keydown();
+        //$timeout.flush();
+        expect($timeout.verifyNoPendingTasks).not.toThrow();
+    });
+
+    it("should cancel timout on search", function () {
+        $this = $controller("SearchController", { $location: $location, $timeout: $timeout }, { query: "star wars" });
+        $this.keyup();
+        $this.search();
+        
+        expect($timeout.verifyNoPendingTasks).not.toThrow();
     });
 });
--- src/movie-app/search.controller.js
+++ src/movie-app/search.controller.js
@@ -1,9 +1,19 @@
 ﻿angular.module("movieApp")
-    .controller("SearchController", function ($location) {
+    .controller("SearchController", function ($location,$timeout) {
         var searchController = this;
+        var timeout;
         searchController.search = function () {
+            $timeout.cancel(timeout);
             if (searchController.query) {
                 $location.path("/results").search("q", searchController.query);
             }
-        }
-    });
+        };
+
+        searchController.keyup = function () {
+            timeout = $timeout(searchController.search(), 1000);
+        };
+
+        searchController.keydown = function () {
+            $timeout.cancel(timeout);
+        };
+    }); 
